8f95eeac10d5e796bdb2e3c09cfca5844996aa17
hwangenhh <hieu68099@gmail.com>
2024-12-30 19:32:30 +0700

    add

---
+++ TH01_HoangAnhPT/TH01VD/b1.py
@@ -0,0 +1,18 @@
+# Nhập số kẹo
+n = int(input("Nhập vào số kẹo của cô: "))
+
+# Nhập số học sinh
+m = int(input("Nhập vào số học sinh trong lớp: "))
+
+# Kiểm tra số học sinh có lớn hơn 0 không
+if m > 0:
+    # Tính số kẹo mỗi học sinh nhận được
+    candies_per_student = n // m
+    # Tính số kẹo còn lại
+    remaining_candies = n % m
+
+    # In kết quả
+    print(f"Mỗi học sinh được nhận {candies_per_student} cái kẹo.")
+    print(f"Cô còn lại {remaining_candies} cái kẹo.")
+else:
+    print("Số học sinh phải lớn hơn 0.")
